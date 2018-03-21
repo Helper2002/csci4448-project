d1038c7db4a79deac0fc1e4b02b97736e3b9fb0a
Helper2002 <jake.broadwater@gmail.com>
2018-03-20 23:19:31 -0600

    more UI

--- Account.cpp
+++ Account.cpp
@@ -1,13 +1,19 @@
 #include <gtk/gtk.h>
 
-static void display() {
-	g_print("Login Screen\n");
+static void disp() {
+	//g_print("Login Screen?\n");
+}
+
+static void on_click_login(GtkButton *button,gpointer dat) {
+	
 }
 
 static void display(GtkApplication *a, gpointer u_dat) {
 	GtkWidget *window;
 	GtkWidget *button;
 	GtkWidget *button_box;
+	GtkWidget *logbox;
+	GtkWidget *newbox;
 	
 	window = gtk_application_window_new(a);
 	gtk_window_set_title (GTK_WINDOW(window), "Login Screen\n");
@@ -16,13 +22,22 @@ static void display(GtkApplication *a, gpointer u_dat) {
 	button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
     gtk_container_add (GTK_CONTAINER (window), button_box);
     
-    button = gtk_button_new_with_label ("Login Screen\n");
-    g_signal_connect (button, "clicked", G_CALLBACK (display), NULL);
-    g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+    newbox=gtk_box_new(GTK_ORIENTATION_HORIZONTAL,5);
+    gtk_container_add(GTK_CONTAINER(window),newbox);
+
+    logbox=gtk_entry_new();
+    gtk_box_pack_start(GTK_BOX(newbox),logbox,TRUE,TRUE,5);
+    
+    button = gtk_button_new_with_label ("Enter Username\n");
+    g_signal_connect (button, "clicked", G_CALLBACK (on_click_logic), logbox);
+    //g_signal_connect_(button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+    //on_click_login(button, u_dat);
     gtk_container_add (GTK_CONTAINER (button_box), button);
     
     gtk_widget_show_all (window);
 }
+
+
 
 int main(int argc, char **argv) {
 	GtkApplication *a;
